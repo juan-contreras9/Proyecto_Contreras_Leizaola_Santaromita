7d74e9953b9c1cf1674bd36fd74e54ca8a07fc58
Juan Luis Contreras Fernández <juan.contreras@correo.unimet.edu.ve>
2025-08-08 13:02:10 -0400

    Update Artista.py

--- Artista.py
+++ Artista.py
@@ -1,1 +1,13 @@
+class Artista:
+    def __init__ (self, nombre, nacionalidad, fecha_de_nacimiento, fecha_de_muerte):
+        self.nombre=nombre
+        self.nacionalidad=nacionalidad
+        self.fecha_de_nacimiento=fecha_de_nacimiento
+        self.fecha_de_muerte=fecha_de_muerte
+
+    def show(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Nacionalidad: {self.nacionalidad}")
+        print(f"Fecha de nacimiento: {self.fecha_de_nacimiento}")
+        print(f"Fecha de muerte: {self.fecha_de_muerte}")
 
