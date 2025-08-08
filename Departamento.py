6ed960a5c6b456789292cff0968bfca41675c7c7
ste1801 <stefanie.santaromita@correo.unimet.edu.ve>
2025-08-08 17:33:38 -0400

    Update Departamento.py

--- Departamento.py
+++ Departamento.py
@@ -1,1 +1,9 @@
+class Departamento:
+  def __init__ (self, id, nombre):
+    self.id=id
+    self.nombre=nombre
+
+  def show(self):
+    print(f"Id: {self.id}")
+    print(f"Nombre del departamento: {self.nombre}")
 
